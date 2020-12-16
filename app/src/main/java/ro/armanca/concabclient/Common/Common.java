caad51bdfbb38ee4794ed7470feb719f3eadad76
Darius <armancadarius@gmail.com>
2020-12-16 20:27:38 +0200

    Vizualizare soferi cu iconita in zona

--- app/src/main/java/ro/armanca/concabclient/Common/Common.java
+++ app/src/main/java/ro/armanca/concabclient/Common/Common.java
@@ -12,19 +12,23 @@ import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import ro.armanca.concabclient.Model.AnimationModel;
 import ro.armanca.concabclient.Model.ClientModel;
 import ro.armanca.concabclient.Model.DriverGeoModel;
 import ro.armanca.concabclient.R;
 
 public class Common {
-    public static final String CLIENT_INFO_REFERENCE ="ClientInfo";
+    public static final String CLIENT_INFO_REFERENCE = "ClientInfo";
     public static final String TOKEN_REFERENCE = "Token";
     public static final String NOTI_CONTENT = "body";
     public static final String NOTI_TITLE = "title";
@@ -34,17 +38,16 @@ public class Common {
     public static ClientModel currentClient;
     public static Set<DriverGeoModel> driversFound = new HashSet<DriverGeoModel>();
     public static HashMap<String, Marker> markerList = new HashMap<>();
+    public static HashMap<String, AnimationModel> driverLocationSubscribe = new HashMap<String, AnimationModel>();
 
     public static String buildWelcomeMessage() {
 
-        if(Common.currentClient!=null){
+        if (Common.currentClient != null) {
             return new StringBuilder("Bine ai venit ")
                     .append(Common.currentClient.getFirstName())
                     .append(" ")
                     .append(Common.currentClient.getLastName()).toString();
-        }
-        else
-        {
+        } else {
             return "";
         }
 
@@ -52,18 +55,17 @@ public class Common {
 
     public static void showNotification(Context context, int id, String title, String body, Intent intent) {
         PendingIntent pendingIntent = null;
-        if(intent != null)
-            pendingIntent = PendingIntent.getActivity(context,id,intent,PendingIntent.FLAG_UPDATE_CURRENT);
-        String NOTIFICATION_CHANNEL_ID="ADR_ConCabClient";
-        NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O)
-        {
-            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID,"ConCabClient",
+        if (intent != null)
+            pendingIntent = PendingIntent.getActivity(context, id, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        String NOTIFICATION_CHANNEL_ID = "ADR_ConCabClient";
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "ConCabClient",
                     NotificationManager.IMPORTANCE_HIGH);
             notificationChannel.setDescription("ConCabClient");
             notificationChannel.enableLights(true);
             notificationChannel.setLightColor(Color.RED);
-            notificationChannel.setVibrationPattern(new long[]{0,100,500,1000});
+            notificationChannel.setVibrationPattern(new long[]{0, 100, 500, 1000});
             notificationChannel.enableVibration(true);
 
             notificationManager.createNotificationChannel(notificationChannel);
@@ -75,15 +77,62 @@ public class Common {
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setDefaults(NotificationCompat.DEFAULT_VIBRATE)
                 .setSmallIcon(R.drawable.concab_faviconpng)
-                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(),R.drawable.concab_faviconpng));
-        if(pendingIntent !=null){
+                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.concab_faviconpng));
+        if (pendingIntent != null) {
             builder.setContentIntent(pendingIntent);
         }
-        Notification notification= builder.build();
-        notificationManager.notify(id,notification);
+        Notification notification = builder.build();
+        notificationManager.notify(id, notification);
     }
 
     public static String buildName(String firstName, String lastName) {
-        return  new StringBuilder(firstName).append(" ").append(lastName).toString();
+        return new StringBuilder(firstName).append(" ").append(lastName).toString();
+    }
+
+    public static List<LatLng> decodePoly(String encoded) {
+        List poly = new ArrayList();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng p = new LatLng((((double) lat / 1E5)),
+                    (((double) lng / 1E5)));
+            poly.add(p);
+        }
+        return poly;
+    }
+
+    public static float getBearing(LatLng begin, LatLng end) {
+        double lat = Math.abs(begin.latitude - end.latitude);
+        double lng = Math.abs(begin.longitude - end.longitude);
+
+        if (begin.latitude < end.latitude && begin.longitude < end.longitude)
+            return (float) (Math.toDegrees(Math.atan(lng / lat)));
+        else if (begin.latitude >= end.latitude && begin.longitude < end.longitude)
+            return (float) ((90 - Math.toDegrees(Math.atan(lng / lat))) + 90);
+        else if (begin.latitude >= end.latitude && begin.longitude >= end.longitude)
+            return (float) (Math.toDegrees(Math.atan(lng / lat)) + 180);
+        else if (begin.latitude < end.latitude && begin.longitude >= end.longitude)
+            return (float) ((90 - Math.toDegrees(Math.atan(lng / lat))) + 270);
+        return -1;
     }
 }
